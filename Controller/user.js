ead004854c707cb4c42147371a178b789eaa00c6
bhavesh029 <chouhanbhavesh9@gmail.com>
2022-04-07 22:09:46 +0530

    Working with Frontend using axios(signup)

--- Controller/user.js
+++ Controller/user.js
@@ -1,6 +1,5 @@
 const bcrypt = require('bcrypt');
 
-
 const User = require('../Models/users');
 
 exports.signUp = (req, res, next) =>{
@@ -9,20 +8,16 @@ exports.signUp = (req, res, next) =>{
     const contact = req.body.contact;
     const password = req.body.password;
 
-    const saltRounds = 10;
-    bcrypt.genSalt(saltRounds, (err, salt) => {
-        bcrypt.hash(password, (err, hash) => {
-            if(err){
-                console.log("Unable to create the user")
-                res.json({message: 'Unabl to create the user'})
-            }
-            User.create({name, email, contact, password: hash})
-                .then(() => {
-                    res.status(201).json({message: 'Successfully created the new user'})
-                })
-                .catch(err => {
-                    res.status(403).json(err);
-                })
-        });
-    });
+    User.create({name, email, contact, password})
+        .then(() => {
+            res.status(201).json({message: 'Successfully created the new user'})
+        })
+        .catch(err => {
+            res.status(403).json(err);
+        })
+};
+
+
+exports.login = (req, res, next) => {
+
 }
--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.json());
+app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded());
 
 app.use('/user',userRoutes);
