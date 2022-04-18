4337c1e014ead562dde011ce3821b524e3234e97
bhavesh029 <chouhanbhavesh9@gmail.com>
2022-04-18 17:31:21 +0530

    adding forget password frontend

---
+++ Views/ForgetPassword/forgetPassword.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+    var btn = document.getElementById('password-btn');
+    btn.addEventListener('click', (e)=> {
+        e.preventDefault();
+        var email = document.getElementById('email').value;
+        
+        var userDetails ={
+            email:email
+        }
+        console.log(userDetails);
+        axios.post('http://localhost:3000/password/forgetpassword', userDetails).then(response => {
+        if(response.status === 200){
+            document.body.innerHTML+= '<div style="color:red;">Mail Successfully Sent </div>'
+        }else{
+            throw new Error('Something Went Wrong');
+        }
+        }).catch(err => {
+            document.body.innerHTML+= `<div style="color:red;">${err} </div>`
+        })
+    })
+})
